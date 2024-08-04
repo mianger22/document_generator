eefca2014cfd1c5dba2bd0104fab82ab0e00aff9
mianger22 <test666777888999@yandex.ru>
2024-08-04 08:45:38 +0300

    created a dynamic description of the patrol route

--- index.js
+++ index.js
@@ -202,7 +202,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const Date_patrol_task__act = document.getElementById("Date_patrol_task__act").value;
         const Senior_patroller__act = document.getElementById("Senior_patroller__act").value;
         const Junior_patroller__act = document.getElementById("Junior_patroller__act").value;
-        const Patrol_route_number__act = document.getElementById("Patrol_route_number__act").value;
+        let Patrol_route_number__act = document.getElementById("Patrol_route_number__act").value;
         const Patrol_report__act = document.getElementById("Patrol_report__act").value;
         const Is_there_photo_table = document.querySelector("#Is_there_photo_table").checked === true
         ? " (приказ директора от _____._____._____ года № _____)" : "";
@@ -241,8 +241,39 @@ document.addEventListener("DOMContentLoaded", () => {
                     break;
             }
 
+            // Создание описания маршрута патрулирования
+            switch (Patrol_route_number__act) {
+                case '70':
+                    Patrol_route_number__act = 'квартал 128 (выдела 14,17,26,30,36,38), квартал 126 (выдела 8,7,15), квартал 228 (выдела 13,21,25), квартал 216 (выдела 17,18,19,23,30), квартал 213 (выдела 20,24,16), квартал 211 (выдела 20,21,24,30,32), квартал 199 (выдела 21,22,31,33,35), квартал 12 (выдела 7,10), квартал 13 (выдела 1,23,40,35)';
+                    break;
+                case '71':
+                    Patrol_route_number__act = 'квартал 200 (выдела 49,50,51,4,6,7), квартал 191 (выдела 25,26,27), квартал 40 (выдела 19,20), квартал 201 (выдела 6,8,14,15,18,24,26,28,31), квартал 195 (выдела 11,12), квартал 196 (выдела 2,10,11,12)';
+                    break;
+                case '72':
+                    Patrol_route_number__act = 'квартал 45 (выдела 7,9), квартал 30 (выдела 12,15,17,19), квартал 192 (выдела 3,13), квартал 82 (выдела 5,6,14,20,25), квартал 100 (выдела 14,19,21), квартал 105 (выдела 3,10,14,17,18), квартал 110 (выдела 9,8,17)';
+                    break;
+                case '73':
+                    Patrol_route_number__act = 'квартал 229 (выдела 2,4,8,9,10,12,13,6,7,11), квартал 116 (выдела 9,13,15,7,18,5,11,12,16), квартал 117 (выдела 18,15,12,16,17,18,21), квартал 119 (выдела 7,16,3,2,17,6,13,14,15)';
+                    break;
+                case '74':
+                    Patrol_route_number__act = 'квартал 240 (выдела 1,2,3,4,5,6,13,11,37,38,41,27,29), квартал 250 (выдела 2,3,5), квартал 257 (выдела 1,47,57), квартал 251 (выдела 1,3), квартал 252 (выдела 23,22,24,18,19)';
+                    break;
+                case '75':
+                    Patrol_route_number__act = 'НЕТ ИНФОРМАЦИИ';
+                    break;
+                case '76':
+                    Patrol_route_number__act = 'НЕТ ИНФОРМАЦИИ';
+                    break;
+                case '77':
+                    Patrol_route_number__act = 'НЕТ ИНФОРМАЦИИ';
+                    break;
+                case '78':
+                    Patrol_route_number__act = 'НЕТ ИНФОРМАЦИИ';
+                    break;
+            }
+
             // Загрузка заранее определенного файла, лежащего в той же директории
-            fetch('Акт задания на проведение патрулирования.docx')
+            fetch('Шаблон акта о проведённом патрулировании.docx')
                 .then(response => {
                     if (!response.ok) {
                         throw new Error('Network response was not ok ' + response.statusText);
