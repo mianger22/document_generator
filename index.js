4914f9d2ab59651fbf242e8df026a31fa9a1dc75
mianger22 <test666777888999@yandex.ru>
2024-08-10 14:26:45 +0300

    refactored

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// -- make pwa --
-
+// Запуск PWA-режима
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('service-worker.js')
